b54f8a7839673a2e6954d6bf7632534850ffdccb
Rahul Maisuria <21282947@student.uwl.ac.uk>
2018-04-24 14:28:54 +0100

    edited layout refactored names

--- Mud0.5.rkt
+++ Mud0.5.rkt
@@ -1,5 +1,5 @@
-
 #lang racket
+
 
 (require srfi/1)
 (require srfi/13)
@@ -8,6 +8,19 @@
 
 (define X 5)
 (define Y 5)
+
+(define search '(((directions) search) ((look) search)((search) search) ((examine room) search)))
+(define quit '(((exit game) quit) ((quit game) quit) ((exit) quit) ((quit) quit)))
+(define pick '(((get) pick ) ((pickup) pick) ((pick) pick)))
+(define directions '(((down) direction) ((up) direction) ((left) direction) ((right) direction)))
+(define put '(((put) drop) ((drop) drop) ((place) drop) ((remove) drop)))
+(define inventory '(((inventory) inventory) ((bag) inventory)))
+(define actions `(,@search ,@quit ,@pick ,@put ,@inventory,@directions))
+
+
+(define decisiontable `((1 ,@actions)
+                        (2 ((down) 1) ,@actions )
+                        (3 ,@actions)))
 
 (define room-type '((0 "Entrance")
                     (1 "sand room")
@@ -27,111 +40,104 @@
                   (5 "a fishing rod")
                   (6 "a maraca")))
 
+(define keys '((0 "a crow bar")
+               (1 "a car key")
+               (2 "a house key")
+               (3 "an exit key")))
 
-(define descriptions '((1 "You are in the castle entrance, you see the toilets to the west and kitchen to the east and castle hallway to the north")
-                       (2 "You are in the castle toilets, you see the entrance to your west. ")
-                       (3 "You are in the castle kitchen, you see the entrance to your east.")
-                       (4 "You are in the castle hallway, you see the entrance to your south and living room to your north")
-                       (5 "You are in the castle living room, you see the castle hallway to your south and dining room to your east")
-                       (6 "You are in the castle dining room, you see the living room to your west and games room to your north")
-                       (7 "You are in the games room, you see the dining room to your south and living room to your west")))
-
-(define search '(((directions) search) ((look) search)((search) search) ((examine room) search)))
-(define quit '(((exit game) quit) ((quit game) quit) ((exit) quit) ((quit) quit)))
-(define pick '(((get) pick ) ((pickup) pick) ((pick) pick)))
-(define directions '(((down) direction) ((up) direction) ((left) direction) ((right) direction)))
-(define put '(((put) drop) ((drop) drop) ((place) drop) ((remove) drop)))
-(define inventory '(((inventory) inventory) ((bag) inventory)))
-(define actions `(,@search ,@quit ,@pick ,@put ,@inventory,@directions))
-
-
-(define decisiontable `((1 ,@actions)
-                        (2 ((down) 1) ,@actions )
-                        (3 ,@actions)))
+(define (paths start)
+  (match-define (maze N M tbl) m)
+  (map (lambda (x)
+         (let ((first (map = start x))
+               (second (map < start x)))
+           (cond [(car first)
+                  (if (cadr second) 'down ' up)]
+                 [else
+                  (if (car second) 'right 'left)]) ))
+       (connections tbl start)))
+                                
+(struct maze (N NM tbl))
+(define (connections tbl c) (dict-ref tbl c '()))
 
 
+(define (connect! tbl c n)
+  (dict-set! tbl c (cons n (connections tbl c)))
+  (dict-set! tbl n (cons c (connections tbl n))))
 
- (define (slist->string l)
-  (string-join (map symbol->string l)))
+(define (connected? tbl a b) (member a (connections tbl b)))
 
-( define ( assq-ref assqlist id )
-   ( cadr ( assq id assqlist )))
-( define rooms ( make-hash ))
-( define ( room-allocator db types )
-   ( for (( j X ))
-      ( for (( i Y ))
-         ( hash-set! db ( list j i) ( assq-ref types ( random (- ( length types ) 1)))))))
-( room-allocator rooms room-type )
+;;;;;;;;;;;;;;;;
+(define (build-maze M N)
+  (define tbl (make-hash))
+  (define (visited? tbl c) (dict-has-key? tbl c))
+  (define (neigbours c)
+    (filter
+     (match-lambda [(list i j) (and (<= 0 i (- N 1)) (<= 0 j (- M 1)))])
+     (for/list ([d '((0 1) (0 -1) (-1 0) (1 0))]) (map + c d))))
+  (let move-to-cell ([c (list (random N) (random M))])
+    (for ([n (shuffle (neigbours c))] #:unless (visited? tbl n))
+      (connect! tbl c n)
+      (move-to-cell n)))
+  (maze N M tbl))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-( struct maze ( N M tbl ))
- (define ( connections tbl c) ( dict-ref tbl c '()))
-( define ( connect! tbl c n )
-   ( dict-set! tbl c ( cons n ( connections tbl c )))
-   ( dict-set! tbl n ( cons c ( connections tbl n ))))
-( define ( connected? tbl a b) ( member a ( connections tbl b )))
-
-( define ( paths start )
-   ( match-define ( maze X Y tbl ) m)
-   ( map ( lambda ( x)
-            ( let (( first ( map = start x ))
-                   ( second ( map < start x )))
-               ( cond [( car first )
-                      ( if ( cadr second ) ' down ' up )]
-                      [ else
-                        ( if ( car second ) ' right ' left )]) ))
-         ( connections tbl start )))
-
-( define ( build-maze N M)
-   ( define tbl ( make-hash ))
-   ( define ( visited? tbl c) ( dict-has-key? tbl c ))
-   ( define ( neigbours c)
-      ( filter
-        ( match-lambda [( list i j) ( and ( <= 0 i (- N 1)) ( <= 0 j (- M 1)))])
-        ( for/list ([ d '((0 1) (0 -1) (-1 0) (1 0))]) ( map + c d ))))   
-   ( let move-to-cell ([ c ( list ( random N) ( random M ))])
-      ( for ([ n ( shuffle ( neigbours c ))] #:unless ( visited? tbl n ))
-         (connect! tbl c n)
-         ( move-to-cell n )))
-   ( maze N M tbl ))
-
-(define (show-maze m pos )
+(define (show-maze m pos)
   (match-define (maze X Y tbl) m)
   (for ([i X]) (display "+---"))
   (displayln "+")
   (for ([j Y])
-     (display "|")
-     (for ([ i (- X 0)])
-        (if ( equal? ( list i j ) pos )
-             ( display " *")
-             ( display "  " ))
-        (if ( connected? tbl ( list i j ) ( list (+ 1 i) j ))
-             ( display "  " )
-             ( display " |" )))
-     (newline )
-     (for ([i X])
-        (if ( connected? tbl ( list i j ) ( list i (+ j 1)))
-             (display "+   ")
-             (display "+---" )))
-     (displayln "+" )))
+    (display "|")
+    (for ([i (- X 0)])
+      (if (equal? (list i j) pos)
+          (display " *")
+          (display "  "))
+      (if (connected? tbl (list i j) (list (+ 1 i ) j))
+          (display "  ")
+          (display " |")))
+    (newline)
+    (for ([i X])
+      (if (connected? tbl (list i j) (list i (+ j 1)))
+          (display "+   ")
+          (display "+---")))
+    (displayln "+")))
 
 
- 
- (define ( add-object db id object )
-  ( if ( hash-has-key? db id )
-       ( let (( record ( hash-ref db id )))
+(define (look room input)
+               (cond [(eq? input 'down)
+                      (move-x room +)]
+                     [(eq? input 'up)
+                      (move-x room -)]
+                     [(eq? input 'left)
+                      (move-y room -)]
+                     [(eq? input 'right)
+                      (move-y room +)]))
+
+(define (move-x room fun)
+  (cons (car room) (map (lambda(x) (fun x 1)) (cdr room))))
+
+(define (move-y room fun)
+  (cons (fun (car room) 1) (cdr room)))
+
+
+
+
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;object functions;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+( define ( add-object db id object )
+   ( if ( hash-has-key? db id )
+        ( let (( record ( hash-ref db id )))
            ( hash-set! db id ( cons object record )))
         ( hash-set! db id ( cons object empty ))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;object functions
-(define ( add-objects db )
+(define (add-objects db)
   (for-each
    (lambda (r)
-     (add-object db (first r) (second r )))objects))
-(add-objects objectdb)
-
-
+     (add-object db (first r) (second r))) objects))
 
 (define (display-objects db id)
   (cond ((hash-has-key? db id)
@@ -179,18 +185,17 @@
 (define (display-inventory)
   (display-objects inventorydb 'bag))
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define objectdb (make-hash)) 
 (define inventorydb (make-hash)) 
-;(define rooms (make-hash))
+(define rooms (make-hash))
 (define m (build-maze X Y)) 
 (define key "")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-;;;;;;;;;;;;;;;;key functions;;;;;;;;;;;;;;;;;;
 (define (ass-ref assqlist id x)
   (cdr (x id assqlist)))
 
@@ -245,9 +250,7 @@
                #t))))
         (else
          #f)))
-
- 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (random-allocator rooms room-type 100)       
 (random-allocator objectdb objects 50)     
 (random-key-location objectdb keys)
@@ -258,24 +261,7 @@
   (list start_x start_y)))
 
 
-(define (look room input)
-               (cond [(eq? input 'down)
-                      (move-x room +)]
-                     [(eq? input 'up)
-                      (move-x room -)]
-                     [(eq? input 'left)
-                      (move-y room -)]
-                     [(eq? input 'right)
-                      (move-y room +)]))
-
-(define (move-x room fun)
-  (cons (car room) (map (lambda(x) (fun x 1)) (cdr room))))
-
-(define (move-y room fun)
-  (cons (fun (car room) 1) (cdr room)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+;;;;;;;;;;;;;;;;;;;;main loop;;;;;;;;;;;;;;;;;;;;
 (define (startgame room-id)
   (let* ((key (car (ass-ref keys (random(length keys)) assq)))
          (X(random X))
@@ -333,4 +319,3 @@
 
 
 (startgame maze)
-
